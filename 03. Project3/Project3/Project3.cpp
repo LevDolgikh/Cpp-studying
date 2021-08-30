f2d0330530158321397397599b1f531899d042a9
Лев <88022074+LevDolgikh@users.noreply.github.com>
2021-08-30 11:13:58 +0700

    Add files via upload

---
+++ 03. Project3/Project3/Project3.cpp
@@ -0,0 +1,34 @@
+﻿// Date: 30.08.2021
+// Created by: Lev Dolgikh 
+
+
+// Проект #3
+// Рассматриваются циклы while, do-while и for
+
+#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    
+    // Цикл while
+    int a = 0;
+    while (a < 10) 
+    {
+        std::cout << "Переменная a = " << a << std::endl;
+        a++;
+    }
+
+    // Цикл do-while
+    do
+    {
+        std::cout << "Переменная a = " << a << std::endl;
+        a++;
+    } while (a < 20);
+
+    // Цикл for
+    for (int i = 0; i < 5; i++)
+    {
+        std::cout << "Переменаня цикла for i = " << i << std::endl;
+    }
+}
